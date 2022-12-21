7a0ec64bad576a1207e93e165e450d92608f6ad0
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2022-12-21 13:57:38 +0200

    end

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,29 @@
 let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
+  $('.slider-gallery').slick({
+    slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
-    dots: true,
-    infinite: true
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+{
+  breakpoint: 851,
+  settings: {
+    slidesToShow: 2,
+  }
+},{
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 1,
+  }
+}
+    ]
   });
+});
+
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
 });
 
