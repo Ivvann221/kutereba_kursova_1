fc27b7f274aa292a8c438f5d83d292cc51aa565b
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-12 17:43:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let slider = $(document).ready(function () {
+  $('.slider-gallery').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+{
+  breakpoint: 851,
+  settings: {
+    slidesToShow: 2,
+  }
+},{
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 1,
+  }
+}
+    ]
+  });
+});
+
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
+});
+$(".menu-collapsed").click(function() {
+  $(".header__content_logo").toggleClass("header-active");
+});
+
+
